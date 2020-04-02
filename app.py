da412ba34e4504a1ee478e6bfdf3e7a7c98e546e
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-04-02 08:03:03 -0700

    Added webhooks

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, make_response, request, render_template, Respo
 from mongoengine import *
 import os
 from flask_cors import CORS, cross_origin
+from functools import wraps
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
@@ -12,14 +13,22 @@ connection = connect('main-db',
         port=27017
         )
 
+def validate_contentful(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        secret = request.headers.get('secret')
+        print('Secret', secret)
+        return func(*args, **kwargs)
+    return decorated
+
 @app.route('/', methods=['GET'])
 def root():
     return 'Change Maker API for CMS '
 
 @app.route('/hooks', methods=['POST'])
+@validate_contentful
 def publish():
-    print(request.json, flush=True)
-    
+    print(request.json)
     return {'ok': 'ok'}
 
 app.run('0.0.0.0', '8080', debug=True)
